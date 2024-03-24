cac9a6241ccc4b4ef5a476ddb6992607d06b39f3
BrianTahiel <briang.2012@hotmail.com>
2024-03-24 16:33:06 -0300

    adding new folder/firebase

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { useState } from 'react';
-import { auth } from '../firebaseCredentials';
+import { auth } from '../config/firebaseCredentials';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import Link from 'next/link';
 
