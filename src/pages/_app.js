8ac6cfba785d5d1c08a49ded1a5f432364fc7751
BrianTahiel <briang.2012@hotmail.com>
2024-03-25 19:33:11 -0300

    Adding Bootstrap

---
+++ src/pages/_app.js
@@ -0,0 +1,18 @@
+// pages/_app.js
+import 'bootstrap/dist/css/bootstrap.css';
+
+import "../styles/movies.css";
+import { useEffect } from 'react';
+
+
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if (typeof document !== 'undefined') {
+      require('bootstrap/dist/js/bootstrap.bundle.min.js');
+    }
+  }, []);
+
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
--- src/pages/movies.tsx
+++ src/pages/movies.tsx
@@ -12,7 +12,6 @@ import {
 } from "firebase/firestore";
 import { db } from "../firebaseConfig";
 import { auth } from "../firebaseConfig";
-import '../styles/movies.css';
 
 
 const API_KEY = "d0a97fc052097018bb41a342cb55b9b8";
